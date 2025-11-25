c82561366fbe2c08679eb18852320318b32ce6b3
Jia Jia <angersax@sina.com>
2025-11-25 16:33:51 +0800

    refactor(chore): remove badge plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths";
-import { traeBadgePlugin } from 'vite-plugin-trae-solo-badge';
 import { execSync } from 'node:child_process'
 import { readFileSync } from 'fs'
 import { join } from 'path'
@@ -77,15 +76,6 @@ export default defineConfig({
           'react-dev-locator',
         ],
       },
-    }),
-    traeBadgePlugin({
-      variant: 'dark',
-      position: 'bottom-right',
-      prodOnly: true,
-      clickable: true,
-      clickUrl: 'https://www.trae.ai/solo?showJoin=1',
-      autoTheme: true,
-      autoThemeTarget: '#root'
     }),
     tsconfigPaths()
   ],
